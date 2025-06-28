17d7196ac18ebae5ddd43b89fca25a966fcfa396
sun-ji054 <[seonji04203645@gmail.com]>
2025-06-28 14:40:18 +0900

    refactor: 쓸모 없는 주석들 제거

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Opening from './components/Opening';
-//import opening_background from './opening_background.jpg'
 import Blogbar from './components/Blogbar';
-//import Box from './components/Box';
-//import ProfileImg from './components/ProfileImg';
-//import Info from "./components/Info";
 import { Routes, Route} from 'react-router-dom';
 import ListPage from "./pages/ListPage";
 import WritePage from "./pages/WritePage";
 import MainPage from "./pages/MainPage";
-// import LoginPage from "./pages/LoginPage";
-//import loginAPI from "./loginAPI";
-//import api from "./api";
 
 
 export default function App() {
   const [showOpening, setShowOpening] = useState(true);
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const navigate = useNavigate();
 
 useEffect(() => {
     if (showOpening) {
@@ -36,18 +27,7 @@ useEffect(() => {
       </div>
     );
   }
-// if (!isLoggedIn) {
-//     return (
-//       <div className="appContainer">
-//         <LoginPage
-//           onLoginSuccess={() => {
-//             setIsLoggedIn(true);
-//             navigate("/", { replace: true });
-//           }}
-//         />
-//       </div>
-//     );
-//   }
+
 if (showOpening) {
     return (
       <div className="appContainer">
